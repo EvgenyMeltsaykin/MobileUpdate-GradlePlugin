4e4ab7bc0a2ba7c81a2e7c38a87c2ea42cbb9873
evgenymeltsaykin <e.meltsaykin@napoleonit.com>
2023-07-03 18:15:57 +0500

    build project without gradle plugin

--- feature/feature2/build.gradle.kts
+++ feature/feature2/build.gradle.kts
@@ -1,7 +1,44 @@
 plugins {
-    id("convention.feature")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.emeltsaykin.feature.feature"
+    namespace = "com.emeltsaykin.feature.feature2"
+
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 26
+        targetSdk = 33
+    }
+
+    buildFeatures.apply {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation(projects.common.baseFeature)
+    implementation("androidx.activity:activity-compose:1.3.1")
 }
--- feature/feature3/build.gradle.kts
+++ feature/feature3/build.gradle.kts
@@ -1,24 +1,23 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.emeltsaykin.konturmobileupdategradleplugin"
+    namespace = "com.emeltsaykin.feature.feature3"
 
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.emeltsaykin.konturmobileupdategradleplugin"
-
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
-        getByName("release") {
+        release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
@@ -35,11 +34,10 @@ android {
 }
 
 dependencies {
-    implementation(projects.common.baseFeature)
-    implementation(projects.feature.feature1)
-    implementation(projects.feature.feature2)
-    implementation(projects.feature.feature3)
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.6.0")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
